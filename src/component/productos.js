c2418fe1d97c0dcc48ad3803a572410e603747a9
Leonardo De Los Santos <leo_dls@hotmail.com>
2021-06-10 00:38:38 -0300

    Botones cantidad de Producto

--- src/component/productos.js
+++ src/component/productos.js
@@ -1,78 +1,89 @@
-import React from 'react'
 import logo from "./logo192.png"
 import { Card, Col, Button } from 'react-bootstrap';
+import React from 'react';
+import $ from 'jquery';
 
 
 function Producto(props) {
-    const stock=props.stock;
-    function restar(event){
-        if (clicks>1){
-            setClicks(clicks-1);
-            }
-        };
-  
-    function aumentar(event){
-        setClicks(clicks+ 1);
-        };
-    function sendCart(){
-        alert("Agregados "+ clicks);
+    const cart=[""];
+    const stock = props.stock;
+    function restar(event) {
+        if (clicks > 1) {
+            setClicks(clicks - 1);
+        }
+    };
+
+    function aumentar(event) {
+        if (clicks < stock) {
+            setClicks(clicks + 1);
+        }
+
+    };
+    function sendCart() {
+        $("#messege").empty();
+        $("#messege").append(`<h6 class="p-3">${clicks} x ${props.title} agregado al Carrito</h6>`).fadeIn(500).delay(800).fadeOut(800);
+        const pushCart=props.title;
+        cart.push(pushCart);
+        console.log(cart);
+
     }
     const [clicks, setClicks] = React.useState(1);
-    if (stock>0) {
+    if (stock > 0) {
         return (
 
-            <Col className="m-2" xs={12} md={3}>
-                <Card style={{ width: '18rem' }}>
+            <Col className="mt-1" xs={12} md={3}>
+                <Card >
                     <Card.Img variant="top" src={logo} />
                     <Card.Body>
+
                         <Card.Title>{props.title}</Card.Title>
                         <Card.Text>
                             {props.description}
-                            <div>Stock disponible {props.stock}</div>
+                            <div className="text-success">Stock disponible {props.stock}</div>
                         </Card.Text>
-                        
+
                         <div className="row d-flex justify-content-center align-item-center">
                             <button className="col-2 m-1 btn btn-danger rounded" onClick={restar}>-</button>
                             <span className="col-6 m-1 border border-secondary rounded text-center align-item-center">{clicks}</span>
                             <button className="col-2 m-1 btn btn-success rounded" onClick={aumentar} >+</button>
                         </div>
                         
-                        <Button className="mt-3 w-100" variant="primary" onClick={sendCart}>Comprar</Button>
+                        <Button id={props.id} className="mt-3 w-100" variant="primary" onClick={sendCart}>Comprar</Button>
                     </Card.Body>
                 </Card>
             </Col>
-    
+
         );
-    } else {  
+    } else {
         return (
-            
-            <Col className="m-2" xs={12} md={3}>
-                <Card style={{ width: '18rem' }}>
+
+            <Col className="mt-1" xs={12} md={3}>
+                <Card >
                     <Card.Img variant="top" src={logo} />
                     <Card.Body>
                         <Card.Title>{props.title}</Card.Title>
                         <Card.Text>
                             {props.description}
-                            <div>Stock disponible {props.stock}</div>
+                            <div className="text-danger" >Stock disponible {props.stock}</div>
                         </Card.Text>
-                        
-                        
+
+
                         <div className="row d-flex justify-content-center align-item-center">
                             <button className="col-2 m-1 btn btn-secondary rounded" >-</button>
                             <span className="col-6 m-1 border border-secondary rounded text-center align-item-center">0</span>
                             <button className="col-2 m-1 btn btn-secondary rounded" >+</button>
                         </div>
-                        
-                        <Button className="mt-3 w-100"  variant="secondary">Sin Stock</Button>
+
+                        <Button className="mt-3 w-100" variant="secondary">Sin Stock</Button>
                     </Card.Body>
                 </Card>
             </Col>
-    
+
         );
     }
-    
 
-    
+
+
 }
 
 export default Producto;
--- src/index.js
+++ src/index.js
@@ -18,10 +18,14 @@ ReactDOM.render(
 ReactDOM.render(
   <React.StrictMode>
     
-    <Producto title="Producto 1" description="Descripcion de Producto uno" stock="100"/>
-    <Producto title="Producto 2" description="Descripcion de Producto dos" stock="0"/>
-    <Producto title="Producto 3" description="Descripcion de Producto tres" stock="100"/>
-    <Producto title="Producto 4" description="Descripcion de Producto cuatro" stock="0"/>
+    <Producto title="Producto 1" description="Descripcion de Producto numero uno" stock="10"/>
+    <Producto title="Producto 2" description="Descripcion de Producto numero dos" stock="0"/>
+    <Producto title="Producto 3" description="Descripcion de Producto numero tres" stock="14"/>
+    <Producto title="Producto 4" description="Descripcion de Producto numero cuatro" stock="0"/>
+    <Producto title="Producto 5" description="Descripcion de Producto numero cinco" stock="10"/>
+    <Producto title="Producto 6" description="Descripcion de Producto numero seis" stock="0"/>
+    <Producto title="Producto 7" description="Descripcion de Producto numero siete" stock="14"/>
+    <Producto title="Producto 8" description="Descripcion de Producto numero ocho" stock="0"/>
   </React.StrictMode>,
   document.getElementById('Products')
 )
