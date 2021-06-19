d8b2691b82a83e307aebe55eaa281ccfd351f992
Leonardo De Los Santos <leo_dls@hotmail.com>
2021-06-19 13:59:28 -0300

    acualizar datos menu

--- src/component/menu.js
+++ src/component/menu.js
@@ -29,7 +29,7 @@ function Menu() {
             
 
           </ul>
-          <CartWidget cl={cl}/>
+          <CartWidget />
           <form className="d-flex">
             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
             <button className="btn btn-outline-success" type="submit">Search</button>
